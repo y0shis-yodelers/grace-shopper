18dba06458cb6682391092acc99f55a73b7974c6
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-02 15:32:56 -0600

    fix: add Link and link route to allProducts singleCard map

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import {fetchAllProducts} from '../store/products'
 import ProductCard from './ProductCard'
 
@@ -14,7 +15,9 @@ class AllProducts extends React.Component {
       <div>
         <div className="container">
           {products.map((product) => (
-            <ProductCard key={product.id} productId={product.id} />
+            <Link to={`products/${product.id}`}>
+              <ProductCard key={product.id} productId={product.id} />
+            </Link>
           ))}
         </div>
       </div>
