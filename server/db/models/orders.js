e3585e7763638beb2eb979479cc0235221d1162f
Kush Patel <kushpatel21@gmail.com>
2021-03-02 11:06:09 -0600

    chore: added default value for for isPaid

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -4,19 +4,20 @@ const db = require('../db')
 const Orders = db.define('order', {
   isPaid: {
     type: Sequelize.BOOLEAN,
+    defaultValue: false
   },
   pricePaid: {
     type: Sequelize.FLOAT,
     validate: {
-      min: 0,
-    },
+      min: 0
+    }
   },
   quantity: {
     type: Sequelize.INTEGER,
     validate: {
-      min: 0,
-    },
-  },
+      min: 0
+    }
+  }
 })
 
 module.exports = Orders
