33eba5df6dd5cd436d315fab93235826040460d6
gene <softhaxorz@gmail.com>
2021-03-02 12:58:53 -0800

    feat: add POST create new product route feat: add PUT edit product route

--- server/api/products.js
+++ server/api/products.js
@@ -3,7 +3,7 @@ const {Product} = require('../db/models')
 const {Op} = require('sequelize')
 module.exports = router
 
-// get all products route '/api/product'
+// GET all products route '/api/product'
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll({
@@ -19,17 +19,43 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-// get single product route '/api/product/:productId'
+// GET single product route '/api/product/:productId'
 router.get('/:productId', async (req, res, next) => {
   try {
-    const id = req.params.productId
+    const {productId} = req.params
 
-    if (isNaN(id)) res.status(400).send(id + ' is not a number!')
+    if (isNaN(productId)) res.status(400).send(productId + ' is not a number!')
     else {
-      const product = await Product.findByPk(id)
+      const product = await Product.findByPk(productId)
       res.json(product)
     }
   } catch (err) {
     next(err)
+  }
+})
+
+// POST create new product route '/api/product/'
+router.post('/', async (req, res, next) => {
+  try {
+    const data = req.body
+    const {dataValues} = await Product.create(data)
+
+    res.json(dataValues)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// PUT edit product route
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const data = req.body
+    const {productId} = req.params
+
+    await Product.update({...data}, {where: {id: productId}})
+
+    res.json(data)
+  } catch (error) {
+    next(error)
   }
 })
