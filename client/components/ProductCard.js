837d3186eeb18efeb5f315212724bb11a7fb9198
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-02 15:46:36 -0600

    feat: add productCard

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -8,10 +8,17 @@ class ProductCard extends React.Component {
   }
 
   render() {
+    const {product} = this.props.singleProduct
     return (
       <div>
         <div className="productContainer">
-          {/* stuff here pertaining to item view */}
+          <img src={product.imageUrl} />
+          <div className="productName">{product.name}</div>
+          <div className="productPrice">{product.price}</div>
+          <div className="productInventory">
+            Only {product.inventory} left in stock!
+          </div>
+          <p className="productDescription">{product.description}</p>
         </div>
       </div>
     )
