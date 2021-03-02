3761029c7d1282babee73401a1d604f5eb5ae61e
Zoran Bajic <zoranbajic@pm.me>
2021-03-02 15:32:07 -0600

    feat: add order routes

---
+++ server/api/orders.js
@@ -0,0 +1,46 @@
+const router = require('express').Router()
+const {Orders, Product, Address} = require('../db/models')
+
+// Gets all orders
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await Orders.findAll()
+    res.json(orders)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Gets single orders
+router.get('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Orders.findByPK(req.params.orderId, {
+      include: [{model: Product}, {model: Address}]
+    })
+    res.json(order)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Creates an order
+router.post('/', async (req, res, next) => {
+  try {
+    res.send(await Orders.create(req.body))
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Deletes an order
+router.delete('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Orders.findByPK(req.params.orderId)
+    await order.destroy()
+    res.send(order)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
