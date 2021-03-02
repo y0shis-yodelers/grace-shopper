862b686d4d3581c103f77f23394f7a2fbc565620
Kush Patel <kushpatel21@gmail.com>
2021-03-02 16:37:14 -0600

    feat: add and delete items in cart API routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,50 +7,64 @@ router.get('/:cartID', async (req, res, next) => {
     const items = await Cart.findAll({
       where: {cartID: req.params.cartID}
     })
-    res.send(items)
+    res.json(items)
   } catch (error) {
     next(error)
   }
 })
 
-//Add item to cart
-router.post('/:cartID/product/:productID', async (req, res, next) => {
-  try {
-    const [item, wasCreated] = await Cart.findOrCreate({
-      returning: true,
-      where: {cartID: req.params.cartID, productId: req.params.productId}
-    })
+// //Add item to cart
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const allItems = req.body
+//     await allItems.forEach((item) => {
+//       Cart.create(item)
+//     })
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
-    if (wasCreated) {
-      res.send(item)
-    } else {
-      item.updateQuantity(req.body.amount).save()
-    }
-  } catch (error) {
-    next(error)
-  }
-})
+// //Add item to cart
+// router.post('/:cartID/product/:productID', async (req, res, next) => {
+//   try {
+//     const [item, wasCreated] = await Cart.findOrCreate({
+//       returning: true,
+//       where: {cartID: req.params.cartID, productId: req.params.productID},
+//     })
 
-//edit item quantity in cart
-router.put('/:cartID/product/:productID', async (req, res, next) => {
-  try {
-    const [item, wasCreated] = await Cart.update(req.body, {
-      returning: true,
-      where: {cartID: req.params.cartID, productId: req.params.productId}
-    })
-    res.send(item)
-  } catch (error) {
-    next(error)
-  }
-})
+//     if (wasCreated) {
+//       res.json(item)
+//     } else {
+//       item.updateQuantity(req.body.amount).save()
+//       res.json(item)
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+// //edit item quantity in cart
+// router.put('/:cartID/product/:productID', async (req, res, next) => {
+//   try {
+//     const item = await Cart.findOne({
+//       returning: true,
+//       where: {cartID: req.params.cartID, productId: req.params.productID},
+//     })
+//     item.updateQuantity(req.body.amount).save()
+//     res.json(item)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 //Delete/remove single item in cart
 router.delete('/:cartID/product/:productID', async (req, res, next) => {
   try {
     const numItems = await Cart.destroy({
-      where: {cartID: req.params.cartID, productId: req.params.productId}
+      where: {cartID: req.params.cartID, productId: req.params.productID}
     })
-    res.send(numItems)
+    res.json(numItems)
   } catch (error) {
     next(error)
   }
@@ -62,7 +76,7 @@ router.delete('/:cartID', async (req, res, next) => {
     const numItems = await Cart.destroy({
       where: {cartID: req.params.cartID}
     })
-    res.send(numItems)
+    res.json(numItems)
   } catch (error) {
     next(error)
   }
