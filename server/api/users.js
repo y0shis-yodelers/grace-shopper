38c233984f0ade15ff360c60cec71ec94d6afb4e
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-02 16:06:00 -0600

    fix: users route

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Address} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -11,6 +11,68 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email'],
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET single user
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// POST single user
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// UPDATE single user
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      include: Address,
+    })
+    // where local state in user update form contains:
+    // address: {
+    //   number, houseOrApt, streetName, city, state, zip
+    // }
+    const updateAddressInfo = req.body.address
+    const updateUserInfo = {
+      name: req.body.name,
+      // how to handle the address
+      // as a separate table in this REST route?
+
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+    }
+    // update address without returning
+    await Address.update(updateAddressInfo)
+    // THEN update and return user with modified address
+    const updatedUser = await User.update(updateUserInfo, {
+      returning: true,
+    })
+    res.status(204).send(updatedUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// DELETE single user
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(+req.params.userId)
+    await user.destroy()
+    res.sendStatus(204)
   } catch (err) {
     next(err)
   }
