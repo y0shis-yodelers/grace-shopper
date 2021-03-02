45648f949dbe19d03504de27eec658f35bf5e75e
gene <softhaxorz@gmail.com>
2021-03-02 12:48:39 -0800

    feat: add GET single product route

--- server/api/products.js
+++ server/api/products.js
@@ -18,3 +18,18 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+// get single product route '/api/product/:productId'
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const id = req.params.productId
+
+    if (isNaN(id)) res.status(400).send(id + ' is not a number!')
+    else {
+      const product = await Product.findByPk(id)
+      res.json(product)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
