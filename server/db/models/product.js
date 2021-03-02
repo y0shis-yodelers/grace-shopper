5be94ce836c7ec20aa7e6a00f22dda750556a99c
gene <softhaxorz@gmail.com>
2021-03-02 08:50:27 -0800

    feat: add product model

---
+++ server/db/models/product.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    defaultValue: 'This is a placeholder description.'
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://upload.wikimedia.org/wikipedia/commons/4/4e/Volkswagen_Bus_Pick_Guitar.JPG',
+    validate: {
+      isUrl: true
+    }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  }
+})
+
+module.exports = Product
