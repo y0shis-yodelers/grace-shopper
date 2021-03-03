0a755019232322245df5f045cfdf8f3fbd26e21f
Kush Patel <kushpatel21@gmail.com>
2021-03-03 16:46:55 -0600

    feat: shell for total component

---
+++ client/components/Total.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Total = () => {
+  return (
+    <div>
+      <div>Subtotal</div>
+      <div>Tax</div>
+      <div>Shipping</div>
+      <div>GrandTotal</div>
+    </div>
+  )
+}
+
+export default Total
