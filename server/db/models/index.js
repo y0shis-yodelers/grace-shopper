11782af2c7e8eda9ead08aaa6292bfb959f613aa
Kush Patel <kushpatel21@gmail.com>
2021-03-02 22:33:57 -0600

    fix: fix many to many associations to have through table defined

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,7 @@
 const User = require('./user')
 const Cart = require('./cart')
 const Product = require('./product')
-const Order = require('./order')
+const Order = require('./orders')
 const Address = require('./address')
 
 /**
@@ -21,17 +21,17 @@ const Address = require('./address')
 Order.belongsTo(User)
 User.hasMany(Order)
 
-Product.belongsToMany(Cart)
-Cart.hasMany(Product)
+Product.belongsToMany(Cart, {through: 'Product_Cart'})
+Cart.belongsToMany(Product, {through: 'Product_Cart'})
 
 User.hasOne(Cart)
 Cart.belongsTo(User)
 
-Product.belongsToMany(Order)
-Order.hasMany(Product)
+Product.belongsToMany(Order, {through: 'Product_Order'})
+Order.belongsToMany(Product, {through: 'Product_Order'})
 
-User.hasMany(Address)
-Address.belongsToMany(User)
+User.belongsToMany(Address, {through: 'User_Address'})
+Address.belongsToMany(User, {through: 'User_Address'})
 
 module.exports = {
   User,
