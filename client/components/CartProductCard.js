939be96a09b6277dbc0118673bda5b672b12043a
Kush Patel <kushpatel21@gmail.com>
2021-03-03 16:14:49 -0600

    feat: created cmini component for when item is in cart on sidebar

--- client/components/CartProductCard.js
+++ client/components/CartProductCard.js
@@ -23,6 +23,12 @@ class CartProductCard extends React.Component {
         <div className="cartProductContainer">
           <img src={product.imageUrl} />
           <div className="productName">{product.name}</div>
+          <button type="button" name="addQnty">
+            +
+          </button>
+          <button type="button" name="removeQnty">
+            -
+          </button>
           <div className="productQnty">{product.quantity}</div>
           <div className="productPrice">{product.price}</div>
         </div>
