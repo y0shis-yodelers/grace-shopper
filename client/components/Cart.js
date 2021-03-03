33f70d7ee072c1f480b3219301935601fc14f452
Kush Patel <kushpatel21@gmail.com>
2021-03-03 16:45:41 -0600

    refactor: convert CartProductCart into functional compoenent

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import CartProductCard from './CartProductCard'
 import Total from './Total'
 import Submit from './Submit'
 
 class Cart extends React.Component {
   render() {
+    const products = this.props.cart || []
     return (
       <div>
-        <CartProductCard />
+        {products.map(product => (
+          <div key={product.id}>
+            <CartProductCard product={product} />
+          </div>
+        ))}
         <Total />
         <Submit />
       </div>
@@ -15,4 +21,8 @@ class Cart extends React.Component {
   }
 }
 
-export default Cart
+const mapState = state => ({
+  cart: state.cart
+})
+
+export default connect(mapState, null)(Cart)
--- client/components/CartProductCard.js
+++ client/components/CartProductCard.js
@@ -1,48 +1,22 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchSingleProduct} from '../store/cart'
 
-// temp db here
-let prodIdCounter = 1
-let products = require('../../script/seed/products-seed')
-products.forEach(product => (product.id = ++prodIdCounter))
-console.log(products)
-
-class CartProductCard extends React.Component {
-  render() {
-    // restore this when db functional
-    // const {product} = this.props.singleProduct
-
-    // temp db
-    const product = products.find(
-      product => product.id === this.props.productId
-    )
-
-    return (
-      <div>
-        <div className="cartProductContainer">
-          <img src={product.imageUrl} />
-          <div className="productName">{product.name}</div>
-          <button type="button" name="addQnty">
-            +
-          </button>
-          <button type="button" name="removeQnty">
-            -
-          </button>
-          <div className="productQnty">{product.quantity}</div>
-          <div className="productPrice">{product.price}</div>
-        </div>
+const CartProductCard = ({product}) => {
+  return (
+    <div>
+      <div className="cartProductContainer">
+        <img src={product.imageUrl} />
+        <div className="productName">{product.name}</div>
+        <button type="button" name="addQnty">
+          +
+        </button>
+        <button type="button" name="removeQnty">
+          -
+        </button>
+        <div className="productQnty">{product.quantity}</div>
+        <div className="productPrice">{product.price}</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapState = state => ({
-  cart: state.cart
-})
-
-const mapDispatch = dispatch => ({
-  addToCart: productId => dispatch(fetchSingleProduct(productId))
-})
-
-export default connect(mapState, mapDispatch)(CartProductCard)
+export default CartProductCard
