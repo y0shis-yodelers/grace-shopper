bd376a5c4e58f7dc173eddbac0546553cae25398
Kush Patel <kushpatel21@gmail.com>
2021-03-03 16:03:27 -0600

    chore: add cart reducer to combine function

--- client/store/index.js
+++ client/store/index.js
@@ -3,10 +3,11 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import cart from './cart'
 import products from './products'
 import singleProduct from './singleProduct'
 
-const reducer = combineReducers({user, products, singleProduct})
+const reducer = combineReducers({user, products, singleProduct, cart})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
