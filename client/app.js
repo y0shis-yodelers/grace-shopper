8f8a5c5cb677beb3248b350f20ee35f6a69d9b83
Kush Patel <kushpatel21@gmail.com>
2021-03-02 23:26:35 -0600

    chore: seed file added

--- client/app.js
+++ client/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import {Navbar} from './components'
+import AllProducts from './components/AllProducts'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
+      <AllProducts />
       <Navbar />
       <Routes />
     </div>
