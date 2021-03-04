17155ecd332f06bdb02e56f7c84fa7f4f620715b
Kush Patel <kushpatel21@gmail.com>
2021-03-04 16:01:04 -0600

    fix: cleanup of cart store

--- client/store/cart.js
+++ client/store/cart.js
@@ -6,25 +6,11 @@ const CLEAR_CART = 'CLEAR_CART'
 const SAVE_CART = 'SAVE_CART' //save cart to local storage
 const LOAD_CART = 'LOAD_CART' //save cart to local storage
 
-const addToCart = item => {
-  return {type: ADD_TO_CART, item}
-}
-
-export const removeFromCart = itemId => {
-  return {type: REMOVE_FROM_CART, itemId}
-}
-
-export const clearCart = () => {
-  return {type: CLEAR_CART}
-}
-
-export const saveCart = () => {
-  return {type: SAVE_CART}
-}
-
-export const loadCart = () => {
-  return {type: LOAD_CART}
-}
+export const addToCart = item => ({type: ADD_TO_CART, item})
+export const removeFromCart = itemId => ({type: REMOVE_FROM_CART, itemId})
+export const clearCart = () => ({type: CLEAR_CART})
+export const saveCart = () => ({type: SAVE_CART})
+export const loadCart = () => ({type: LOAD_CART})
 
 export const fetchProductToAdd = productId => {
   return async dispatch => {
@@ -48,6 +34,7 @@ export default (state = [], action) => {
           item => item.productId !== action.item.productId
         )
       }
+
     case CLEAR_CART:
       return {...state, cart: []}
     case SAVE_CART:
