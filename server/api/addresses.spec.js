36aa4fc44861ae92db210bb84382bbc48bc65f2b
Kush Patel <kushpatel21@gmail.com>
2021-03-04 11:05:31 -0600

    test: get all address test completed

---
+++ server/api/addresses.spec.js
@@ -0,0 +1,32 @@
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Address = db.model('address')
+
+describe('Address routes', () => {
+  beforeEach(async () => {
+    await db.sync({force: true})
+
+    const seedAddress = require('../../script/seed/addresses-seed')
+
+    await Promise.all(
+      seedAddress.map(address => {
+        return Address.create(address)
+      })
+    )
+  })
+
+  describe('/api/address/', async () => {
+    it('allows admin users to GET all address', () => {
+      const addresses = request
+        .agent(app)
+        .get('/api/address/')
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body).to.have.lengthOf(10)
+        })
+        .catch(err => console.log('this err happened: ', err))
+    })
+  })
+})
