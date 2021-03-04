9bda218e0e7fdc26e75c1b8964cefef4c2f5623e
gene <softhaxorz@gmail.com>
2021-03-04 09:30:45 -0800

    refactor: api products route test specs (wip)

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -7,90 +7,55 @@ const app = require('../index')
 const Product = db.model('product')
 
 describe('Product routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
+  beforeEach(async () => {
+    await db.sync({force: true})
+
+    const seedProducts = require('../../script/seed/products-seed')
+
+    await Promise.all(
+      seedProducts.map(product => {
+        return Product.create(product)
+      })
+    )
   })
 
   describe('`/products` URI', () => {
-    const products = [
-      {
-        name: 'Very Generic Pick',
-        inventory: 34,
-        price: 100
-      },
-      {
-        name: 'Generic Pick 2',
-        inventory: 22,
-        price: 123
-      },
-      {
-        name: 'Pick your nose',
-        inventory: 34,
-        price: 222
-      },
-      {
-        name: 'Satanic Pick',
-        inventory: 666,
-        price: 666
-      }
-    ]
-
-    const alice = {
-      name: 'Alice',
-      inventory: 5,
-      price: 1212
-    }
-
-    beforeEach(() => {
-      products.forEach(product => {
-        return Product.create(product)
-      })
-    })
-
     it('GET responds with all products', async () => {
-      const res = await request(app)
+      await request(app)
         .get('/api/products')
         .expect(200)
-
-      expect(res.body).to.be.an('array')
-      res.body.forEach((product, index) => {
-        expect(product.name).to.be.equal(products[index].name)
-        expect(product.inventory).to.be.equal(products[index].inventory)
-        expect(product.price).to.be.equal(products[index].price)
-      })
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body).to.have.lengthOf(10)
+        })
     })
 
     it('POST responds with created product', async () => {
-      db.drop()
-      const res = await request(app)
-        .post('/api/products', alice)
-        .expect(200)
-
-      expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('Alice')
+      await request
+        .agent(app)
+        .post('/api/products', {
+          name: 'Alice',
+          inventory: 5,
+          price: 1212
+        })
+        .then(res => {
+          expect(res.body).to.be.an('object')
+          expect(res.body.name).to.be.equal('Alice')
+        })
     })
   })
 
   describe('`/products/:productId` URI', () => {
-    const POD = {
-      name: 'Pick of Destiny',
-      inventory: 1,
-      price: 100000
-    }
-
-    beforeEach(() => {
-      return Product.create(POD)
-    })
-
     it('GET responds with single product', async () => {
-      const res = await request(app)
+      await request(app)
         .get('/api/products/1')
         .expect(200)
-
-      expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal(POD.name)
-      expect(res.body.inventory).to.be.equal(POD.inventory)
-      expect(+res.body.price).to.be.equal(POD.price)
+        .then(res => {
+          expect(res.body).to.be.an('object')
+          expect(res.body.name).to.be.an('string')
+          expect(res.body.inventory).to.be.an('number')
+          expect(+res.body.price).to.be.an('number')
+        })
     })
   })
 })
