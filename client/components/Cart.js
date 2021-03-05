13e299340b551882906ba90ae87d294bbf9f5a2f
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-04 19:03:41 -0600

    update cart to pass down quantity handler to individual cartProductCards

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import CartProductCard from './CartProductCard'
+import {fetchUpdateCart} from '../store/cart'
 
 class Cart extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleQuantityChange = this.handleQuantityChange.bind(this)
+  }
+
+  async handleQuantityChange(productId, quantity) {
+    await this.props.updateCart(productId, quantity)
+  }
+
   render() {
-    const products = this.props.cart || []
+    const products = this.props.cart || {}
+
     return (
       <div>
         {products.map(product => (
@@ -21,4 +32,9 @@ const mapState = state => ({
   cart: state.cart
 })
 
-export default connect(mapState, null)(Cart)
+const mapDispatch = dispatch => ({
+  updateCart: (productId, quantity) =>
+    dispatch(fetchUpdateCart(productId, quantity))
+})
+
+export default connect(mapState, mapDispatch)(Cart)
