bb65fe652a23f4a0b6dec0086b86d0690f94285f
Kush Patel <kushpatel21@gmail.com>
2021-03-05 16:00:20 -0600

    address store better

--- client/store/address.js
+++ client/store/address.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 const GET_SINGLE_ADDRESS = 'GET_SINGLE_ADDRESS'
 
-const getSingleAddress = address => ({
+const getSingleAddress = (name, address) => ({
   type: GET_SINGLE_ADDRESS,
+  name,
   address
 })
 
@@ -11,19 +12,27 @@ export const fetchSingleAddress = userId => {
   return async dispatch => {
     try {
       const {data} = await axios.get(`/api/users/${userId}`)
-      dispatch(getSingleAddress(data))
+      dispatch(getSingleAddress(data.name, data.address))
     } catch (err) {
       console.error(err)
     }
   }
 }
 
-const initState = []
+const initState = {}
 
 export default (state = initState, action) => {
   switch (action.type) {
     case GET_SINGLE_ADDRESS:
-      return [...action.address]
+      return {
+        name: action.name,
+        number: action.number,
+        houseOrApt: action.houseOrApt,
+        streetName: action.streetName,
+        city: action.city,
+        state: action.state,
+        zip: action.zip
+      }
     default:
       return state
   }
