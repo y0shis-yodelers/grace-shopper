2105a192c1145cff6c60b902680d2171b3de6597
Kush Patel <kushpatel21@gmail.com>
2021-03-05 14:43:47 -0600

    fix: fix formatPrice for all price inputs

--- client/components/helperFunctions.js
+++ client/components/helperFunctions.js
@@ -1,9 +1,10 @@
 // convert price in pennies to formatted price in dollars, cents
 // ex., 780 -> $7.80 : 55 -> $0.55 : 9 -> $0.09
 export function formatPrice(price) {
+  price = Math.round(price)
   if (price < 10) return `$0.0${price}`
   if (price < 100) return `$0.${price}`
-  let priceArray = [...price]
+  let priceArray = [...String(price)]
   priceArray.splice(priceArray.length - 2, 0, '.')
   const formattedPrice = priceArray.join('')
   return `$${formattedPrice}`
