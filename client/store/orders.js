7dd9edd40c6ce0fd2f921d95f911422c662d1a9f
gene <softhaxorz@gmail.com>
2021-03-06 11:24:07 -0800

    chore: create orders redux store

---
+++ client/store/orders.js
@@ -0,0 +1,41 @@
+import Axios from 'axios'
+
+// ---- ACTION TYPES ----
+
+const SET_ORDERS = 'SET_ORDERS'
+
+// ---- ACTION CREATORS ----
+
+const getOrders = orders => {
+  return {
+    type: SET_ORDERS,
+    orders
+  }
+}
+
+// ---- THUNKS ----
+
+export const fetchOrders = userId => {
+  return async dispatch => {
+    try {
+      const {data} = await Axios.get(`/api/orders/users/${userId}`)
+
+      dispatch(getOrders(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+// ---- REDUCER ----
+
+const initialState = []
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ORDERS:
+      return action.orders
+    default:
+      return state
+  }
+}
