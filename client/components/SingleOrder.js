fdfe4329d5ea42707617e20c33a7cfbe052e3192
gene <softhaxorz@gmail.com>
2021-03-07 11:04:05 -0800

    chore: convert SingleOrder to a class component

--- client/components/SingleOrder.js
+++ client/components/SingleOrder.js
@@ -1,20 +1,40 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchOrder} from '../store/singleOrder'
 
-const SingleOrder = ({order}) => {
-  return (
-    <div>
-      {order && !order.id ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="orderContainer">
-          <img src={order.imageUrl} />
-          <div className="orderName">{order.name}</div>
-          <div>{order.pricePaid}</div>
-          <div>{order.quantity}</div>
-        </div>
-      )}
-    </div>
-  )
+// const SingleOrder = ({order}) => {
+//   return (
+//     <div>
+//       {order && !order.id ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <div className="orderContainer">
+//           <img src={order.imageUrl} />
+//           <div className="orderName">{order.name}</div>
+//           <div>{order.pricePaid}</div>
+//           <div>{order.quantity}</div>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+class SingleOrder extends React.Component {
+  render() {
+    return <div />
+  }
 }
 
-export default SingleOrder
+const mapState = state => {
+  return {
+    order: state.SingleOrder
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    getOrder: (userId, orderId) => dispatch(fetchOrder(userId, orderId))
+  }
+}
+
+export default connect(mapState, mapDispatch)(SingleOrder)
