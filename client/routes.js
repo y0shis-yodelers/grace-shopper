a8ba95f97899800c90ba39d90c60b2b4a2151075
shapirodaniel <shapirodanieladam@gmail.com>
2021-03-07 05:23:22 -0600

    fix: restore user to mapState

--- client/routes.js
+++ client/routes.js
@@ -90,7 +90,7 @@ const mapDispatch = dispatch => ({
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(null, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Routes))
 
 /**
  * PROP TYPES
