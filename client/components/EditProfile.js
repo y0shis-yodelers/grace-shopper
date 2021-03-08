346606506d122b4622c8987a9477a00a55488252
gene <softhaxorz@gmail.com>
2021-03-08 14:56:01 -0800

    feat: create EditProfile component

---
+++ client/components/EditProfile.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class EditProfile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: this.props.user.name || '',
+      email: this.props.user.email || '',
+      password: '',
+      phone: +this.props.user.phone || ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    const {userId} = this.props.match.params
+    const {name, email, password, phone} = this.state
+    const {handleChange} = this
+
+    return (
+      <form className="shippingData">
+        <div className="formField">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" onChange={handleChange} value={name} />
+        </div>
+
+        <div className="formField">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            onChange={handleChange}
+            value={email}
+          />
+        </div>
+
+        <div className="formField">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+          />
+        </div>
+
+        <div className="formField">
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="text"
+            name="phone"
+            onChange={handleChange}
+            value={phone}
+          />
+        </div>
+
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapState)(EditProfile)
