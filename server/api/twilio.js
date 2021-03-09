777ae2fbb03561c43874cbb4e1147b75dd723798
Kush Patel <kushpatel21@gmail.com>
2021-03-09 15:14:55 -0600

    minor changes to readme and twilio

--- server/api/twilio.js
+++ server/api/twilio.js
@@ -11,13 +11,15 @@ const sendSMS = user => {
   const tomorrow = new Date(today)
   const delivery = `${tomorrow.getMonth() + 1}/${tomorrow.getDate() + 2}`
 
+  const formatted = `+1${user.phoneNumber}`
+
   const client = new twilio(accountSid, authToken)
   client.messages
     .create({
       body: `Thanks for your order ${
         user.name
       }. Expect to be rocking by ${delivery}`,
-      to: '+12243879750', // Text this number
+      to: '+12243879750', // Text this number -> formatted
       from: phone // From a valid Twilio number
     })
     .then(message => console.log(message.sid))
