0b349dca34842fa27b1aae4ab42cc420c34d40b0
gene <softhaxorz@gmail.com>
2021-03-09 12:47:49 -0800

    feat: make form validation errors toast notes

--- client/components/EditProfile.js
+++ client/components/EditProfile.js
@@ -6,14 +6,14 @@ import {notify} from './helperFunctions'
 const validate = (name, email, phone) => {
   let errors = []
 
-  if (!name.length) errors.push('Name must not be empty')
+  if (!name.length) errors.push('Name must not be empty!')
 
-  if (email.length < 5) errors.push('Email must be at least 5 characters long')
-  if (!email.includes('@')) errors.push('Email must contain an @ symbol')
-  if (!email.includes('.')) errors.push('Email must contain at least one dot')
+  if (email.length < 5) errors.push('Email must be at least 5 characters long!')
+  if (!email.includes('@')) errors.push('Email must contain an @ symbol!')
+  if (!email.includes('.')) errors.push('Email must contain at least one dot!')
 
   if (phone.length !== 10) {
-    errors.push('Phone number must be 10 characters long')
+    errors.push('Phone number must be 10 characters long!')
   }
 
   return errors
@@ -26,9 +26,8 @@ class EditProfile extends React.Component {
       id: this.props.user.id || '',
       name: this.props.user.name || '',
       email: this.props.user.email || '',
-      phoneNumber: this.props.user.phoneNumber || '',
+      phoneNumber: this.props.user.phoneNumber || ''
       // password: '',
-      errors: []
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -47,7 +46,7 @@ class EditProfile extends React.Component {
     const {name, email, phoneNumber} = this.state
     const errors = validate(name, email, phoneNumber)
 
-    if (errors.length) this.setState({errors})
+    if (errors.length) errors.forEach(error => notify(error, 'error'))
     else {
       this.props.updateUser(this.state)
       this.props.history.push(`/users/${userId}/`)
@@ -57,17 +56,19 @@ class EditProfile extends React.Component {
 
   render() {
     const {userId} = this.props.match.params
-    const {name, email, phoneNumber, /* password, */ errors} = this.state
+    const {name, email, phoneNumber /* password, */} = this.state
     const {handleChange, handleSubmit} = this
 
     return this.props.user.id && this.props.user.id === +userId ? (
       <div>
         <form className="shippingData" onSubmit={handleSubmit}>
-          {errors.map(error => (
+          {/* {errors.map(error => (
             <p key={error} className="formError">
               Error: {error}
             </p>
-          ))}
+          ))} */}
+
+          {/* {errors.forEach((error) => notify(error, 'error'))} */}
 
           <div className="formField">
             <label htmlFor="name">Name:</label>
