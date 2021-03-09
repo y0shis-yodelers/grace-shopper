7eae55ded17fcf952cde5859fed0795c491cc7d1
Kush Patel <kushpatel21@gmail.com>
2021-03-09 10:47:37 -0600

    added authoer email to cc

--- server/api/email.js
+++ server/api/email.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer')
 
-const pw = process.env.EMAIL_PW || require('../../secrets')
+const pw = process.env.EMAIL_PW || require('../../secrets').pw
+const ccEmail = process.env.CC_EMAIL || require('../../secrets').ccEmail
 
+console.log(pw)
 const email = 'yoshis.yodelers@gmail.com'
 
 const sendEmail = (userEmail, order) => {
@@ -15,8 +17,8 @@ const sendEmail = (userEmail, order) => {
 
   const mailOptions = {
     from: email,
-    to: `${userEmail}, kushpatel21@gmail.com`,
-    subject: 'Bass Shopper Order!',
+    to: `${userEmail}, ` + ccEmail,
+    subject: 'Bass Shopper Order Confirmation!',
     text: `Thank you for your order. It will be shipping soon. Order Number: ${order}`
   }
 
