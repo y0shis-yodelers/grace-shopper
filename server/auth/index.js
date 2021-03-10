f09f026f3c9cf56623c981992eb4a12264857d9e
Kush Patel <kushpatel21@gmail.com>
2021-03-09 20:56:37 -0600

    fix: new users have a blank cart

--- server/auth/index.js
+++ server/auth/index.js
@@ -25,6 +25,12 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
+    const newOrder = await Order.create({
+      isPaid: false,
+      userId: user.id
+    })
+
+    user.hasOrder(newOrder)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
